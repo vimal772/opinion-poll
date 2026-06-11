b4a5e273dc707f0ce9be53ac5e5414a1559874ee
Vimal <Vimal115077286@gmail.com>
2026-06-11 20:36:46 +0530

    Add useActionState and basic form validation

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,23 +1,79 @@
+import { useActionState } from "react";
+
+function newOpinionAction(prevValue, formData) {
+  const name = formData.get('userName')
+  const title = formData.get('title')
+  const body = formData.get('body')
+
+  const errors = []
+
+  if (!name) errors.push('Please enter a valid Name')
+  if (!title) errors.push('Please eneter a valida title')
+  if (!body) errors.push('Please eneter a valid opinion')
+
+  if (errors.length) return {
+    errors, eneterValues: {
+      name,
+      title,
+      body
+    }
+  }
+
+  return {
+    errors: null
+  }
+}
+
 export function NewOpinion() {
+
+  const [formData, formAction] = useActionState(newOpinionAction, {
+    errors: null
+  })
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              required
+              defaultValue={formData.eneterValues?.name || ''} />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              required
+              defaultValue={formData.eneterValues?.title || ''} />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            required
+            defaultValue={formData.eneterValues?.body || ''}></textarea>
         </p>
+
+        { formData.errors && (
+          <ul className="errors">
+            {
+              formData.errors.map((item, index) => {
+                return <li key={index}>{item}</li>
+              })
+            }
+          </ul>
+        )}
 
         <p className="actions">
           <button type="submit">Submit</button>
